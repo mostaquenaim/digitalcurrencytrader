30c48fbe6b80bd73fbddd7821594513cf73e0f7c
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2023-05-12 02:02:41 +0600

    ltst

--- pages/Admin/component/advisorData.js
+++ pages/Admin/component/advisorData.js
@@ -1,18 +1,18 @@
 
-import Image from 'next/image'
+// import Image from 'next/image'
 
-export default function AdminLayout({data})   
-{
-    return(
-        <>
+// export default function AdminLayout({data})   
+// {
+//     return(
+//         <>
       
-      <h1>Firstname: {data.Firstname}</h1>
-      <h1>Lastname: {data.Lastname}</h1>
-      <h1>Email: {data.Email}</h1>
-      <h1>Username: {data.Username}</h1>
-      <h1>Mobile no: {data.Mobile}</h1>
-      <h1>Picture: </h1>
-      <Image src={"http:/localhost:3000/admin/getadvisorimage/"+data.filename} alt="me" width="150" height="150" />
-        </>
-    )
-}
+//       <h1>Firstname: {data.Firstname}</h1>
+//       <h1>Lastname: {data.Lastname}</h1>
+//       <h1>Email: {data.Email}</h1>
+//       <h1>Username: {data.Username}</h1>
+//       <h1>Mobile no: {data.Mobile}</h1>
+//       <h1>Picture: </h1>
+//       <Image src={"http:/localhost:3000/admin/getadvisorimage/"+data.filename} alt="me" width="150" height="150" />
+//         </>
+//     )
+// }
