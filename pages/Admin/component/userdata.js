02d98001210b035ed20c76b39abd78992ea20929
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2023-05-12 02:17:43 +0600

    ltst

--- pages/Admin/component/userdata.js
+++ pages/Admin/component/userdata.js
@@ -1,16 +1,16 @@
 
-// import Image from 'next/image'
+import Image from 'next/image'
 
-// export default function UserLayout({data})   
-// {
-//     return(
-//         <>
+export default function UserLayout({data})   
+{if(!data) return null
+    return(
+        <>
       
-//       {/* <h1>Name: {data.name}</h1> */}
-//       <h1>Email: {data.email}</h1>
-//       <h1>Username: {data.username}</h1>
-//       <h1>Picture: </h1>
-//       <Image src={"http:/localhost:3000/admin/getcustomerimage/"+data.file} alt="me" width="150" height="150" />
-//         </>
-//     )
-// }
+      <h1>Name: {data.name}</h1>
+      <h1>Email: {data.email}</h1>
+      <h1>Username: {data.username}</h1>
+      <h1>Picture: </h1>
+      <Image src={"http:/localhost:3000/admin/getcustomerimage/"+data.file} alt="me" width="150" height="150" />
+        </>
+    )
+}
