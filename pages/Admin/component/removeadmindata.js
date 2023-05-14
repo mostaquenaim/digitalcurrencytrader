aa3c0d37816ea081eafcd16edc2677f55f5db343
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2023-05-15 02:20:23 +0600

    done

--- pages/Admin/component/removeadmindata.js
+++ pages/Admin/component/removeadmindata.js
@@ -50,13 +50,11 @@ export default function RemoveAdminData({ data }) {
       <div className="flex flex-col items-center">
     <p className="text-red-500 font-bold">{success}</p>
 
-        <div className="w-48 h-48 rounded-full overflow-hidden shadow-lg">
-
-          <Image src={`https://nestjs-production-0acd.up.railway.app/admin/getimage/${data.filename}`} alt="me" width="150" height="150" />
-        </div>
+        
         <h1 className="text-3xl font-bold mb-4">Name: {data.name}</h1>
         <h1 className="text-3xl font-bold mb-4">Email: {data.email}</h1>
         <h1 className="text-3xl font-bold mb-4">Username: {data.uname}</h1>
+        
         <button
           onClick={handleButtonSubmit}
           className="px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-600"
