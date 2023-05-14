64b6d51da6f1621426341a6d098e98748ff3dcf2
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2023-05-15 02:29:42 +0600

    done

--- pages/Admin/component/removeadmindata.js
+++ pages/Admin/component/removeadmindata.js
@@ -1,60 +1,42 @@
+
 import Image from 'next/image'
-import axios from 'axios'
-import { useState } from "react"
 
+export default function RemoveAdminData({data})   
+{
+    if(!data) return null
 
-export default function RemoveAdminData({ data }) {
-
-
-    const [email, setEmail] =useState("")
-    const [success, setSuccess] = useState('')
-
-
-  const handleButtonSubmit = async () => {
-    try {
-    //   await axios.delete('https://nestjs-production-0acd.up.railway.app/admin/deleteAdminByEmail', {
-    //     data: { email: data.email },
-    //   });
-    console.log(data)
-        setEmail(data.email)
-        console.log(email)
-
-        const result = await axios.delete('https://nestjs-production-0acd.up.railway.app/admin/deleteAdminByEmail', {
-            params: { email: data.email }, 
-          });
-
-          if(result) {
-            setSuccess("Admin deleted")
-          }
-
+    const handleButtonSubmit = async () => {
+        try {
+       
+    
+            const result = await axios.delete('https://nestjs-production-0acd.up.railway.app/admin/deleteAdminByEmail', {
+                params: { email: data.email }, 
+              });
+    
+              if(result) {
+                setSuccess("Admin deleted")
+              }
+    
+            
         
-    
-    } catch (error) {
-      console.error('Error deleting admin:', error);
-      // Handle error cases
-    }
-  };
+        } catch (error) {
+          console.error('Error deleting admin:', error);
+          
+        }
+      };
 
-//   const adminemail=sessionStorage.getItem('email')
-//         setEmail(adminemail)
-
-//         const result = await axios.get(`https://nestjs-production-0acd.up.railway.app/admin/profile`, {
-//             params: { email: adminemail }, 
-//           });
-    
-//         setUser(result.data);
-
-  return (
-    <>
+    return(
+      <>
 
       <div className="flex flex-col items-center">
-    <p className="text-red-500 font-bold">{success}</p>
 
-        
+        <div className="w-48 h-48 rounded-full overflow-hidden shadow-lg">
+
+          <Image src={`https://nestjs-production-0acd.up.railway.app/admin/getimage/${data.filename}`} alt="me" width="150" height="150" />
+        </div>
         <h1 className="text-3xl font-bold mb-4">Name: {data.name}</h1>
         <h1 className="text-3xl font-bold mb-4">Email: {data.email}</h1>
         <h1 className="text-3xl font-bold mb-4">Username: {data.uname}</h1>
-        
         <button
           onClick={handleButtonSubmit}
           className="px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-600"
@@ -63,5 +45,6 @@ export default function RemoveAdminData({ data }) {
         </button>
       </div>
     </>
-  );
+    )
 }
+
